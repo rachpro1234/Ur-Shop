7a7c1f3373761747d10fae49db294a5ea4a8efd6
Rachid El Barqouqy <108005388+rachpro1234@users.noreply.github.com>
2024-03-21 17:38:10 +0000

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, {lazy} from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Home from "./home/Home.jsx"
--- src/newsletter/Newsletter.jsx
+++ src/newsletter/Newsletter.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import "./newsletter.css"
-
+import newsletterCover from "../img/banner/b1.jpg"
 
 const Newsletter = () => {
   return (
-    <section id="newsletter" className="newsletter">
-            <div className="newsletter-info">
-                <h3>sign up for newsletter</h3>
-                <p>get e-mail updates about our latest shop and <span>special offers.</span></p>
+    <section className="newsletter">
+            <img src={newsletterCover} alt="" />
+            <div className="newsletter-content">
+                <div className="newsletter-info">
+                    <h3>sign up for newsletter</h3>
+                    <p>get e-mail updates about our latest shop and <span>special offers.</span></p>
+                </div>
+                <form action="" className="form">
+                    <input type="text" placeholder='your email address' />
+                    <button className='newsletter-btn' type='submit'>sign up</button>
+                </form>
             </div>
-            <form action="" className="form">
-                <input type="text" placeholder='your email address' />
-                <button className='newsletter-btn' type='submit'>sign up</button>
-            </form>
     </section>
   )
 }
