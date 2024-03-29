bcb79120695909b0a222e0eda3cbafaa3913f0b5
Rachid El Barqouqy <108005388+rachpro1234@users.noreply.github.com>
2024-03-29 17:16:12 +0000

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -12,64 +12,88 @@ import Signup from "./components/signup/Signup.jsx";
 import Login from "./components/login/Login.jsx";
 import Signout from "./components/signout/Signout.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromChildren,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import { CartProvider } from "react-use-cart";
 
 // code splitting
 
 const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/header",
-          element: <Header />,
-        },
-        {
-          path: "/blog",
-          element: <Blog />,
-        },
-        {
-          path: "/cart",
-          element: <Cart />,
-        },
-        {
-          path: "/contact",
-          element: <Contact />,
-        },
-        {
-          path: "/shop",
-          element: <Shop />,
-        },
-        {
-          path: "/about",
-          element: <About />,
-        },
-        {
-          path: "/signup",
-          element: <Signup />,
-        },
-        {
-          path: "/login",
-          element: <Login />,
-        },
-        {
-          path: "/signout",
-          element: <Signout />,
-        },
-      ],
-    },
-  ],
+  // createRoutesFromElement
+  createRoutesFromChildren(
+    <Route path="/" element={<App />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/header" element={<Header />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/about"element={<About />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signout" element={<Signout />} />
+    </Route>
+  )
 );
+
+// const router = createBrowserRouter(
+//   [
+//     {
+//       path: "/",
+//       element: <App />,
+//       children: [
+//         {
+//           path: "/",
+//           element: <Home />,
+//         },
+//         {
+//           path: "/header",
+//           element: <Header />,
+//         },
+//         {
+//           path: "/blog",
+//           element: <Blog />,
+//         },
+//         {
+//           path: "/cart",
+//           element: <Cart />,
+//         },
+//         {
+//           path: "/contact",
+//           element: <Contact />,
+//         },
+//         {
+//           path: "/shop",
+//           element: <Shop />,
+//         },
+//         {
+//           path: "/about",
+//           element: <About />,
+//         },
+//         {
+//           path: "/signup",
+//           element: <Signup />,
+//         },
+//         {
+//           path: "/login",
+//           element: <Login />,
+//         },
+//         {
+//           path: "/signout",
+//           element: <Signout />,
+//         },
+//       ],
+//     },
+//   ],
+// );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <CartProvider>
-      <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </CartProvider>
 );
